89f722cfd12a79f777dfc617c4df4d102b1d9168
Rajko V <rajko.v7686@gmail.com>
2022-11-05 15:16:42 +0000

    Release 1.0.0-alpha.21+21

--- main.dart.js_10.part.js
+++
@@ -1,6 +0,0 @@
-self.$__dart_deferred_initializers__=self.$__dart_deferred_initializers__||Object.create(null)
-$__dart_deferred_initializers__.current=function(a,b,c,$){var A,B
-A=c[19]
-B=c[0]
-var z=a.updateTypes([]);(function constants(){A.tX=new B.a0(10,10,10,10)})()}
-$__dart_deferred_initializers__["5Mp/fy1RelButZBMXJx/5UY/jLg="] = $__dart_deferred_initializers__.current

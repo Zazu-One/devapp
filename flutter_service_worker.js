4c06ad155e6e06b7bf791ed110c7e761504751e9
Rajko V <rajko.v7686@gmail.com>
2022-11-05 01:04:18 +0000

    Release 1.0.0-alpha.21+21, branch ZAZU-T-48, commit id d1a400a129f258646dd3077a3c9fff467f2715ea

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "c8a849bc00a9f03f678ad9c4e4e8f84a",
-"index.html": "abcc13620ff9a4f703e45235682d9899",
-"/": "abcc13620ff9a4f703e45235682d9899",
-"main.dart.js": "a7ada09e81297518696b2c7beb4ea47c",
+"index.html": "9ffa63db8bbfd674fc16c9861a1a4667",
+"/": "9ffa63db8bbfd674fc16c9861a1a4667",
+"main.dart.js": "66f101d9b0e5190b8aa14f134c3080f0",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -16,7 +16,7 @@ const RESOURCES = {
 "manifest.json": "70eb68538b3c6fb62ea55f4061b2f821",
 "assets/AssetManifest.json": "53fefb97216cd7a6dd02cad719060cc9",
 "assets/NOTICES": "fe111c9829c53783d397ae30a533318e",
-"assets/env": "a8fcf38cc1425d75a44ac5352a155fae",
+"assets/env": "fdfa1395fc30912190baa90a86878ec6",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/packages/wakelock_web/assets/no_sleep.js": "7748a45cd593f33280669b29c2c8919a",
